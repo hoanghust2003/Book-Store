a29e10bfbaa3f6ed5ddbf76b87bebbd4db4f4c8d
HoangNguyen263 <108289803+HoangNguyen263@users.noreply.github.com>
2024-10-20 21:21:40 +0700

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
 
 import { Outlet } from 'react-router-dom'
 import './App.css'
-
+import Navbar from './components/Navbar';
 function App() {
   
 
   return (
     <>
-    <nav>Nabar</nav>
-    <main className='min-h-screen max-w-screen-2x1 mx-auto px-4 py-6'>
+    <Navbar></Navbar>
+    <main className='min-h-screen max-w-screen-2x1 mx-auto px-4 py-6 font-primary' >
       <Outlet/>
       </main>
     <footer>Footer</footer>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#FFCE1A',
+        'secondary': '#0D0842',
+        'blackBack': '#F3F3F3',
+        'Favourite': '#FF5841'
+      },
+      fontFamily: {
+        'primary': ["Montserrat", "sans-serif"],
+        'secondary': ["Nunito Sans", "sans-serif"]
+      }
+    },
   },
   plugins: [],
 }
