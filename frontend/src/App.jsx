f7785624d72b58c7a6794edd55c546e2aba9e2ac
HoangNNVipro <hotentennguoidung123456789@gmail.com>
2024-10-20 15:18:59 +0700

    setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
     <nav>Nabar</nav>
-    <main className='min-h-screen max-w-screen-2x1 mx-auto px-4 py-6'>
+    <main className='min-h-screen max-w-screen-2x1 mx-auto px-4 py-6 font-primary'>
       <Outlet/>
       </main>
     <footer>Footer</footer>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,8 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors : {
+        'primary': '#FFCE1A',
+        'secondary': "#000842",
+        'blackBG': '#F3F3F3',
+        'Favorite': '#FF5841'
+      },
+      fontFamily: {
+        'primary' : ["Montserrat", "sans-serif"],
+        'secondary' : ["Nunito Sans", "sans-serif"]
+      }      
+    },
   },
   plugins: [],
 }
+
 
