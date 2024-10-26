40f2515980dad4aa2ecaa07e52459d895482966f
HoangNguyen263 <108289803+HoangNguyen263@users.noreply.github.com>
2024-10-26 23:48:21 +0700

    update

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Banner from './Banner'
-
+import Recommened from './Recommened'
+import TopSellers from './TopSellers'
 const Home = () => {
   return (
     <>
         <Banner/>
-      
+        <TopSellers/>
+        
+        
     </>
   )
 }
--- frontend/src/pages/home/Recommened.jsx
+++ frontend/src/pages/home/Recommened.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
