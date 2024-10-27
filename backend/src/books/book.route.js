bb144af90dbdb56145754127cace68f363e0e71a
cowboiz <dovanhoang1201@gmail.com>
2024-10-27 15:12:01 +0700

    import verifyToken

--- backend/src/books/book.route.js
+++ backend/src/books/book.route.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const Book = require('./book.model');
 const { postABook, getAllBooks, getSingleBook, UpdateBook, deleteABook } = require('./book.controller');
+const verifyAdminToken = require('../middleware/verifyAdminToken');
 
 const router =  express.Router();
 
